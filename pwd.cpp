88bf8547334cb876a04b095fea1615ae8281332c
Josbin <joshyjosbin@gmail.com>
2026-06-14 21:56:17 +0530

    Update pwd.cpp

--- pwd.cpp
+++ pwd.cpp
@@ -1,14 +1,12 @@
 #include<iostream>
 #include<filesystem>
-#include"styles.h"
 #include<string>
 
 namespace fs = std::filesystem;
-namespace tc = color;
 
 void pwd(const fs::path& CurrentPath)
 {
-    std::cout<<tc::BRIGHT_BLUE<<"Current Directory: ";
-    std::cout<<CurrentPath.string()<<tc::RESET;
+    std::cout<<"Current Directory: ";
+    std::cout<<CurrentPath.string();
     std::cout<<std::endl;
 }
